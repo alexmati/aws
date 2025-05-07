851fa67faf751e08ac0750239d87a2320c4195f7
Alex Mati <alexmati100@gmail.com>
2025-05-07 22:21:10 +0100

    feat: create OUs for sandbox, preprod and prod accounts

--- aws-organisations/main.tf
+++ aws-organisations/main.tf
@@ -1,3 +1,21 @@
+# Create an AWS Organization
 resource "aws_organizations_organization" "org" {
   feature_set = "ALL"
+}
+
+resource "aws_organizations_organizational_unit" "sandbox" {
+  name      = "sandbox"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+# Create Pre-production Organizational Unit
+resource "aws_organizations_organizational_unit" "preprod" {
+  name      = "preprod"
+  parent_id = aws_organizations_organization.org.roots[0].id
+}
+
+# Create Production Organizational Unit
+resource "aws_organizations_organizational_unit" "prod" {
+  name      = "production"
+  parent_id = aws_organizations_organization.org.roots[0].id
 }

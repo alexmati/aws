563d6f97e6b36c75efb480dbba7889d70ce58d35
Alex Mati <alexmati100@gmail.com>
2025-11-27 20:52:07 +0000

    refactor: update bucket name for s3 backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "organizations-terraform-state"
+    bucket       = "organizations-terraform-state-${data.aws_caller_identity.current}"
     key          = "terraform.tfstate"
     region       = "eu-west-2"
     use_lockfile = true

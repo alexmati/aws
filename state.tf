fc536c593498a889df9415b371cc3b814cdfba85
Alex Mati <alexmati100@gmail.com>
2025-11-27 21:20:37 +0000

    feat: remove s3 bucket policy

--- state.tf
+++ state.tf
@@ -18,34 +18,3 @@ resource "aws_s3_bucket_public_access_block" "example" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
-
-resource "aws_s3_bucket_policy" "terraform_state" {
-  bucket = data.aws_s3_bucket.organizations_state.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          AWS = data.aws_iam_role.deployment.arn
-        }
-        Action   = "s3:ListBucket"
-        Resource = data.aws_s3_bucket.terraform_state.arn
-      },
-      {
-        Effect = "Allow",
-        Principal = {
-          AWS = data.aws_iam_role.deployment.arn
-        }
-        Action = [
-          "s3:GetObject",
-          "s3:PutObject"
-        ]
-        Resource = [
-          "${data.aws_s3_bucket.terraform_state.arn}/*"
-        ]
-      }
-    ]
-  })
-}

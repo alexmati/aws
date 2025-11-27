56cf12dd522d54fb5ece8119e15c903bcfcb34a5
Alex Mati <alexmati100@gmail.com>
2025-11-27 20:50:57 +0000

    feat: block all public access on s3 bucket

--- state.tf
+++ state.tf
@@ -1,21 +1,26 @@
-resource "aws_s3_bucket" "organizations_state" {
-  bucket = "organizations-terraform-state"
-
-  lifecycle {
-    prevent_destroy = true
-  }
+data "aws_s3_bucket" "organizations_state" {
+  bucket = "organizations-terraform-state-${data.aws_caller_identity.current}"
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.organizations_state.id
+  bucket = data.aws_s3_bucket.organizations_state.id
 
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = data.aws_s3_bucket.organizations_state.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 resource "aws_s3_bucket_policy" "terraform_state" {
-  bucket = aws_s3_bucket.organizations_state.id
+  bucket = data.aws_s3_bucket.organizations_state.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -26,7 +31,7 @@ resource "aws_s3_bucket_policy" "terraform_state" {
           AWS = data.aws_iam_role.deployment.arn
         }
         Action = "s3:ListBucket"
-        Resource = aws_s3_bucket.terraform_state.arn
+        Resource = data.aws_s3_bucket.terraform_state.arn
       },
       {
         Effect = "Allow",
@@ -38,7 +43,7 @@ resource "aws_s3_bucket_policy" "terraform_state" {
           "s3:PutObject"
         ]
         Resource = [
-          "${aws_s3_bucket.terraform_state.arn}/*"
+          "${data.aws_s3_bucket.terraform_state.arn}/*"
         ]
       }
     ]
